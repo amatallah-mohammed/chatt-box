5c9c2493203715e2d94f42ccfcbb001ef54268e2
amatallah-mohammed <59635815+amatallah-mohammed@users.noreply.github.com>
2020-02-17 11:11:55 +0300

    Add files via upload

---
+++ indeex.js
@@ -0,0 +1,21 @@
+﻿var webSocket=require('ws');
+var server=webSocket.Server;
+var socket=new server({port:3350});
+socket.on("connection",function (ws) {
+    ws.send("welcome");
+    console.log("connected ggg");
+    ws.on("message",(message)=>{
+        var info=JSON.parse(message);
+        socket.clients.forEach((c)=>{
+            if(ws!=c)
+            c.send(info.sender+"<span style=' width: 80%;\n" +
+                "            padding: 15px;\n" +
+                "            margin: 5px 10px 0;\n" +
+                "            background: #1ddced;\n" +
+                "            border-radius: 10px;\n" +
+                "            color: #fff;\n" +
+                "            font-size: 18px;'>"+info.data+"</span>");
+        })
+    })
+
+});
